fc3a720c4fa85816440cd84ac6b1da8579b66abd
Mangamer21 <112113598+Mangamer21@users.noreply.github.com>
2024-06-22 13:49:59 +0200

    Add files via upload

---
+++ wp-content/cache/wpfc-minified/4e3ebxl/a1hxv.js
@@ -0,0 +1,27 @@
+// source --> https://aquadroom.be/wp-content/plugins/advanced-business-hours/js/abh-ajax.js?ver=6.4.2 
+jQuery(function ($) {
+    count = 0;
+    all_texts = new Array();
+    $('.abh_ajax_widgets').each(function () {
+        $widget = $(this);
+        $widget.attr('id', 'abh_widget' + count);
+        var text = $widget.attr('data-text');
+
+        if (text !== null && typeof text !== 'undefined')
+            all_texts[count] = text;
+
+        if ($('.abh_ajax_widgets').length == count + 1) {
+            $.post(abh_ajax.ajax_url, {
+                action: 'abh_ajax_widget',
+                data: all_texts,
+                ajax_nonce : abh_ajax.ajax_nonce
+            }, function (response) {
+                response = $.parseJSON(response);
+                for (i = 0; i <= response.length; i++) {
+                    $('#abh_widget' + i).html(response[i]);
+                }
+            });
+        }
+        count++;
+    });
+});
